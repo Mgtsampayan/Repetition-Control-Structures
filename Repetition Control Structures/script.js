1bea70e7fa69cbb274f1245460936ac7658ee698
Mgtsampayan <90001725+Mgtsampayan@users.noreply.github.com>
2022-11-26 21:57:40 +0800

    Add files via upload

---
+++ Repetition Control Structures/script.js
@@ -0,0 +1,12 @@
+let number = prompt("Give me a number");
+
+number = parseInt(number);
+console.log(number);
+
+if (number <= 50) {
+   alert("Currently at number." + number + " Terminating the loop");
+}
+else {
+   alert("THANK YOU SO MUCH Don't go over 50 Practice Some More");
+}
+
